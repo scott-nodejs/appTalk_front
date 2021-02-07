5c2d2039f2c770fae2b19279807a8c4215a1b397
lucong <lucong@kingsoft.com>
2021-02-07 14:03:52 +0800

    update : 修改配置

--- code/server/constants.js
+++ code/server/constants.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
   proxy: {
-    '/api': 'http://39.105.231.100:8080/',
+    '/api': 'http://www.appshuo.club//api-admin/v1/',
+    // '/api': 'http://39.105.231.100:8080/',
     // '/node': 'http://169.254.94.44:8088/',
   },
 }
